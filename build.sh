b20763cc8d860ad63cdb793b22e82fc4b77659f2
Yaguang Tang <heut2008@gmail.com>
2022-08-09 11:37:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 sudo apt install python3-pip git
 sudo apt install qemu-utils git kpartx debootstrap
 
-git clone https://opendev.org/openstack/octavia -b master /root/octavia
+git clone https://opendev.org/openstack/octavia -b master ~/octavia
 
 
-cd /root/octavia/diskimage-create
+cd ~/octavia/diskimage-create
 pip3 install -r requirements.txt
 
-chmod +x /root/octavia/diskimage-create/diskimage-create.sh
+chmod +x ~/octavia/diskimage-create/diskimage-create.sh
 
-bash /root/octavia/diskimage-create/diskimage-create.sh -f
+bash ~/octavia/diskimage-create/diskimage-create.sh -f
