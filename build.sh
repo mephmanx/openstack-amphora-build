f92fea609efa3f47c8589522f344ca397e28fa9b
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2022-09-25 12:39:59 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ sudo apt install python3-pip git
+ sudo apt install qemu-utils git kpartx debootstrap
+
+ git clone https://opendev.org/openstack/octavia -b stable/"$OPENSTACK_VERSION" ~/octavia
+
+
+ cd ~/octavia/diskimage-create
+ pip3 install -r requirements.txt
+
+ chmod +x ~/octavia/diskimage-create/diskimage-create.sh
+
+ bash ~/octavia/diskimage-create/diskimage-create.sh -f
